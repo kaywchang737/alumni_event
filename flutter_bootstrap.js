7c5371a4ae4128d95cecf4375ff74031ef95eb90
Kayw Chang <86800830+kaywchangmarma@users.noreply.github.com>
2025-06-12 15:39:19 +0600

    Update site

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2074418873"
+    serviceWorkerVersion: "3701543056"
   }
 });
